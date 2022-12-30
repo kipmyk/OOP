39cf65e0e454d59248e780dcb2ad7c88d6a8acfd
kipmyk <kipmyk@gmail.com>
2022-12-30 09:21:54 +0300

    Updated - PHP Inheritance

--- Inheritance/Inheritance.php
+++ Inheritance/Inheritance.php
@@ -43,3 +43,12 @@ $account->deposit(100);
 $account->setInterestRate(0.05);
 $account->addInterest();
 echo $account->getBalance();
+
+/**
+ * 
+ * Inheritance allows a class to reuse the code of another class without duplicating it.
+ * Use the extends keyword to define one class that inherits from another class.
+ * A class that inherits another class is called a subclass, a child class, or a derived class. The class from which the subclass inherits is a parent class, a superclass, or a base class.
+ * A subclass can have its own properties and methods.
+ * Use $this keyword to call the methods of the parent class from methods in the child class.
+ * */ 
