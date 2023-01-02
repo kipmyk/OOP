5066f57cae354a1acc55e1a7a8b50e29464792ee
kipmyk <kipmyk@gmail.com>
2023-01-02 12:53:15 +0300

    suppressing polymorphic behaviour - updated

--- examples/self-this.php
+++ examples/self-this.php
@@ -2,7 +2,7 @@
 
 /**
  * Use $this to refer to the current object. Use self to refer to the current class. In other words, use  $this->member for non-static members, use self::$member for static members.
- * 
+ * example from @lik https://stackoverflow.com/questions/151969/when-should-i-use-self-over-this
  *  */ 
 
 class Person {
