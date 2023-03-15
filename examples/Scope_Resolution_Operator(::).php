0fc97164604110194807e5ecfa33532d72a6f8c7
kipmyk <kipmyk@gmail.com>
2023-03-15 09:36:06 +0300

    Scope Resolution Operator (::) ¶

---
+++ examples/Scope_Resolution_Operator(::).php
@@ -0,0 +1,26 @@
+<?php
+class MyClass {
+    const CONST_VALUE = 'A constant value';
+}
+
+$classname = 'MyClass';
+echo $classname::CONST_VALUE;
+
+echo MyClass::CONST_VALUE;
+?>
+<?php
+class OtherClass extends MyClass
+{
+    public static $my_static = 'static var';
+
+    public static function doubleColon() {
+        echo parent::CONST_VALUE . "\n";
+        echo self::$my_static . "\n";
+    }
+}
+
+$classname = 'OtherClass';
+$classname::doubleColon();
+
+OtherClass::doubleColon();
+?>
